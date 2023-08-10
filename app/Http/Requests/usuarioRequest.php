de6eb86321bb7c0740ef3a7a5ccebb58fea0688f
sarinhamanu <sarasejbrs@gmail.com>
2023-08-10 13:45:47 -0300

    salvar usuario

--- app/Http/Requests/usuarioRequest.php
+++ app/Http/Requests/usuarioRequest.php
@@ -25,7 +25,7 @@ class usuarioRequest extends FormRequest
     {
         return [
             'nome' => 'required|max:80|min:5',
-            'cpf' => 'required|max:11|min:11',
+            'cpf' => 'required|max:11|min:11|unique:usuarios,cpf',
             'celular' => 'required|max:15|min:10',
             'email' => 'required|email|unique:usuarios,email',
             'password' => 'required|'
@@ -38,5 +38,25 @@ class usuarioRequest extends FormRequest
             'success' => false,
             'error'  => $validator->errors()
         ]));
+    }
+
+    public  function messages()
+    {
+        return [
+            'nome.required' => 'o campo nome e obrigatório',
+            'nome.max' => 'o campo nome deve conter no maximo 80 caracteres',
+            'nome.min' => 'o campo nome deve conter no minimo 5 caracteres',
+            'cpf.required' =>'cpf obrigatório',
+            'cpf.max' => ' cpf deve conter no máximo 11 caracteres',
+            'cpf.min' =>'cpf deve conter no mínino  11 caracteres',
+            'cpf.unique' =>'cpf já cadastrado no sistema',
+            'celular.required'=> 'celular obrigatório',
+            'celular.max' =>'o celular deve conter no máximo 15 caracteres',
+            'celular.min' =>'o celular deve conter no míninimo 10 caracteres',
+            'email.required' =>'E-mail obrigatorio',
+            'email.email' => 'formato de email inválido',
+            'email.unique'=> 'E_mail já cadastrado no sistema',
+            'password.required'=>'senha obrigatória'
+        ];
     }
 }
